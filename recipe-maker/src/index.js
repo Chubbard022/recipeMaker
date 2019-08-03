ef814c5cf6237a6fd82a390c1e629acb876e4bb1
Chubbard022 <chubbard022@gmail.com>
2019-08-03 16:34:12 -0700

    adding import of thunker

--- recipe-maker/src/index.js
+++ recipe-maker/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import thunk from 'redux-thunk';
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
