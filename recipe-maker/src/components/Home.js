fa209388a18b2605da803e34565ac86878d45be4
Chubbard022 <chubbard022@gmail.com>
2019-08-05 16:43:09 -0700

    creating componenent folder and rendering them to App

--- recipe-maker/src/components/Home.js
+++ recipe-maker/src/components/Home.js
@@ -1,3 +1,26 @@
-import react from "React"
+import React,{Component } from "react"
+import '../App.css';
 
-import {connect} from "react-redux"
+class Home extends Component{
+    state = {
+
+    }
+
+    render(){
+        return(
+            <div><nav className = "navbarHome">
+                <a>Login</a>
+                <a>Logout</a>
+                <a>Recipes</a>
+                <a>Calender</a>
+            </nav>
+                <header>
+                    <h1>Dinner Planner</h1>
+                    <p>Dinner planner allows users to create recipes,plan weekly meal ideas all while making cooking and planning going hand in hand</p>
+                </header>
+
+            </div>
+        )}
+}
+
+export default Home
